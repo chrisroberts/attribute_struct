f7c5f1ee868c52d35e242104d74239c81fa56068
Chris Roberts <chrisroberts.code@gmail.com>
2013-07-08 09:04:45 -0700

    Update version for release v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## v0.1.2
+* Fix naming in require
+
 ## v0.1.0
 * Initial release
--- lib/attribute_struct/version.rb
+++ lib/attribute_struct/version.rb
@@ -2,5 +2,5 @@ class AttributeStruct
   class Version < Gem::Version
   end
 
-  VERSION = Version.new('0.1.1')
+  VERSION = Version.new('0.1.2')
 end
