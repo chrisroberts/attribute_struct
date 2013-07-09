b661882e57038bf83318a0c14f796d031b324bf4
Chris Roberts <chrisroberts.code@gmail.com>
2013-07-08 23:47:10 -0700

    Force toplevel namespace

--- lib/attribute_struct/attribute_hash.rb
+++ lib/attribute_struct/attribute_hash.rb
@@ -2,7 +2,7 @@ require 'hashie/extensions/deep_merge'
 require 'hashie/extensions/indifferent_access'
 
 class AttributeStruct
-  class AttributeHash < Hash
+  class AttributeHash < ::Hash
     include Hashie::Extensions::DeepMerge
     include Hashie::Extensions::IndifferentAccess
   end
