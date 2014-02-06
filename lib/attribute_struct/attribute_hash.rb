8d7e95e64a0fd88de5da131a623d323a445ca3d7
Chris Roberts <chrisroberts.code@gmail.com>
2014-02-06 09:08:14 -0800

    Provide proper `to_hash` conversion

--- lib/attribute_struct/attribute_hash.rb
+++ lib/attribute_struct/attribute_hash.rb
@@ -5,5 +5,13 @@ class AttributeStruct
   class AttributeHash < ::Hash
     include ::Hashie::Extensions::DeepMerge
     include ::Hashie::Extensions::IndifferentAccess
+
+    def to_hash
+      ::Hash[
+        self.map do |k,v|
+          [k, v.is_a?(::Hash) ? v.to_hash : v]
+        end
+      ]
+    end
   end
 end
