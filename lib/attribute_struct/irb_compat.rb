3857241c937c8328e2dbfa8c3364b5241605b5e0
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-28 08:18:30 -0700

    Add IRB helper module

---
+++ lib/attribute_struct/irb_compat.rb
@@ -0,0 +1,14 @@
+# Helper methods for IRB interactions
+module IrbCompat
+
+  # @return [String] object inspection
+  def inspect
+    "<[#{self._klass}:#{@table.object_id}] - table: #{@table.inspect}>"
+  end
+
+  # @return [String] string of instance
+  def to_s
+    "<#{self._klass}:#{@table.object_id}>"
+  end
+
+end
