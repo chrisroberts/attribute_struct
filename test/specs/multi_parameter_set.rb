0a7237072240f360b5b7d0ef3a0ded991e5863fc
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-09 14:57:28 -0700

    Add expected coverage for mixed value multi collapse set that is currently failing

--- test/specs/multi_parameter_set.rb
+++ test/specs/multi_parameter_set.rb
@@ -26,5 +26,22 @@ describe AttributeStruct do
       struct['key'].first.must_equal ['value1', 'with_hash' => true]
       struct['key'].last.must_equal ['value2', 'with_hash' => true]
     end
+
+    it 'should allow mixed value multi set when collapsing' do
+      struct = AttributeStruct.new
+      struct._set_state(:value_collapse => true)
+      struct.key 'value1', :with_hash => true
+      struct.key 'value2', :with_hash => true
+      struct.key 'value3'
+      struct.key 'value4'
+      struct.key 'value5', :with_hash => true
+      struct = struct._dump
+      struct['key'][0].must_equal ['value1', 'with_hash' => true]
+      struct['key'][1].must_equal ['value2', 'with_hash' => true]
+      struct['key'][2].must_equal 'value3'
+      struct['key'][3].must_equal 'value4'
+      struct['key'][4].must_equal ['value5', 'with_hash' => true]
+    end
+
   end
 end
