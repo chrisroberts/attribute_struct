6a6b079f8923cb686c785de1f6849835d2445e63
Chris Roberts <code@chrisroberts.org>
2015-08-31 07:03:21 -0700

    When converting keys to string, force CamelString type

--- lib/attribute_struct/attribute_hash.rb
+++ lib/attribute_struct/attribute_hash.rb
@@ -211,7 +211,7 @@ class AttributeStruct
     #
     # @api private
     def convert_key(key)
-      key.kind_of?(Symbol) ? key.to_s : key
+      key.kind_of?(Symbol) || key.kind_of?(String) ? CamelString.new(key.to_s) : key
     end
 
     # @param value<Object> The value to convert.
