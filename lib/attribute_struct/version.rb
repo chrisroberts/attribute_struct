e9fd49d60c8aab613ac2d15ee1f7f407b976f36d
Chris Roberts <code@chrisroberts.org>
2015-09-01 15:02:51 -0700

    Update version for development 0.2.23

--- lib/attribute_struct/version.rb
+++ lib/attribute_struct/version.rb
@@ -2,5 +2,5 @@ require 'attribute_struct/attribute_struct'
 
 class AttributeStruct
   # Current library version
-  VERSION = ::Gem::Version.new('0.2.22')
+  VERSION = ::Gem::Version.new('0.2.23')
 end
