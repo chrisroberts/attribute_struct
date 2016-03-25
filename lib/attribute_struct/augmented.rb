9526a85f7f9fbfd61af30898f3588216a328b8c6
Chris Roberts <code@chrisroberts.org>
2016-03-25 08:39:25 -0700

    Implement expanded AttributeStruct class (#5)

---
+++ lib/attribute_struct/augmented.rb
@@ -0,0 +1,21 @@
+require 'attribute_struct'
+
+class AttributeStruct
+  # AttributeStruct expanded class that include the Kernel module
+  # and automatically objectifies the instance
+  class Augmented < ::AttributeStruct
+
+    include ::Kernel
+
+    # Create a new Augmented AttributeStruct instance. Passes arguments
+    # and block directly to parent for initialization. Automatically
+    # objectifies the instance
+    #
+    # @return [self]
+    def initialize(*args, &block)
+      super(*args, &block)
+      @_objectified = true
+    end
+
+  end
+end
